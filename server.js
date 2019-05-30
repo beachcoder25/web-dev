e30fb2f31789a0702d449bee945010624e72fe8b
Jonah <cornish25@gmail.com>
2019-05-29 20:45:18 -0700

    Express Testing

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+// app.use(express.static('public'));
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
