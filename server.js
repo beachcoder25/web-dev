82e31d254a416af7c328b9884ee6af4d7bf26c81
Jonah <cornish25@gmail.com>
2019-05-29 20:48:12 -0700

    Serving static image

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-
-// app.use(express.static('public'));
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 
