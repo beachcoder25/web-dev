84b8a5d3c55f3c4daa64205d96a38d585bdd9dac
yon <cornish25@gmail.com>
2019-07-29 21:11:45 -0700

    15:49 timestamp, connected Customers

---
+++ reactexpress/client/src/components/customers/customers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './customers.css';
+
+function Customers() {
+  return (
+    <div>
+        <h2>Customers</h2>
+    </div>
+  );
+}
+
+export default Customers;
